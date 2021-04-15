6700774cb81f88292352cd737f7b2087b53d3821
Alex Low <me@alexlow.dev>
2021-04-15 14:09:10 -0400

    added: title to logo field

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,14 +1,18 @@
-import { Box, Container, Flex } from "@chakra-ui/layout"
+import { Box, Container, Flex, Heading } from "@chakra-ui/layout"
 import NavbarLinks from "./NavbarLinks"
 import navRoutes from "@lib/navRoutes"
 import Icon from "@chakra-ui/icon"
+import Link from "@components/NextLink"
 
 const Navbar = () => {
   return (
     <Box boxShadow="md">
       <Container maxW="container.xl">
-        <Flex justifyContent="space-between" alignItems="center">
-          <Icon as="/logo.svg" boxSize={10} />
+        <Flex justify="space-between" align="center">
+          <Link display="flex" alignItems="center">
+            <Icon as="/logo.svg" boxSize={10} />
+            <Heading pl="0.5rem" size="lg">Storyline.Digital</Heading>
+          </Link>
           <NavbarLinks routes={navRoutes} />
         </Flex>
       </Container>
