515421ed16e4e61ca20c708bd2d7c5ff168500ea
Alex Low <me@alexlow.dev>
2021-04-17 04:05:35 -0400

    chore: add page previews in sanity

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
+const STUDIO_REWRITE = {
+  source: "/studio/:path",
+  destination:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3333/studio/:path*"
+      : "/studio/index.html",
+}
+
 module.exports = {
   future: {
     webpack5: false,
   },
+  rewrites: () => [STUDIO_REWRITE],
 }
